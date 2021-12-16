3d3fdf119814474b2a525d6d791fc79f5d4746f8
Thais Luza <thaisluza21@hotmail.com>
2021-12-16 14:47:03 -0300

    solar-system

---
+++ src/components/Missions.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        <div className="cardMissoes">
+          {missions.map((item) => (
+            <MissionCard
+              key={ item.name }
+              name={ item.name }
+              year={ item.year }
+              country={ item.country }
+              destination={ item.destination }
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Missions;
